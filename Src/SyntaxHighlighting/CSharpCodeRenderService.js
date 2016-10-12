7f1f01463364876aebddfba2ed64c8e8ea4c37e4
billsoff <billsoff@hotmail.com>
2016-10-12 20:30:31 +0800

    Update CSharpCodeRenderService.js
    Remove redundant statement

--- Src/SyntaxHighlighting/CSharpCodeRenderService.js
+++ Src/SyntaxHighlighting/CSharpCodeRenderService.js
@@ -1,8 +1,6 @@
 ﻿(function ($) {
     function main() {
         return render(this.getSH(), this.canvas);
-
-        render(this.getSH(), this.canvas);
     }
 
     window['B4CDDB31-1C56-46BB-80F2-225C8B1C475B'] = main;
